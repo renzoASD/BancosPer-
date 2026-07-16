1e6876f5e928f87a9ef18033fb984f8924bc3caa
renzoASD <renzoherrera@blaspascal.edu.pe>
2026-07-16 08:23:49 -0500

    Update CajeroAutomatico.py
    Código modificado para INTERBANK
    Autor: Renzo Herrera

--- CajeroAutomatico.py
+++ CajeroAutomatico.py
@@ -3,14 +3,14 @@
 # Autor: RENZO HERRERA CARHUAMACA
 # ===========================================
 
-saldo = 5000
-pin_correcto = "5678"
+saldo = 20000
+pin_correcto = "9012"
 
 print("===================================")
-print("     BIENVENIDO AL CAJERO BCP")
+print("     CAJERO BANCO INTERBANK")
 print("===================================")
 
-pin = input("Ingrese su clave PIN: ")
+pin = input("Ingrese PIN: ")
 
 if pin == pin_correcto:
 
